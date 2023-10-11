f666c4a6615e1479021ff00185ab27a4f948916e
Rebekah Garris <rebekahlgarris@gmail.com>
2023-10-10 21:47:19 -0500

    working on Task 3, displaySearchResults. Done for the night.

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,16 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +29,16 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+    @PostMapping(value = "")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam(required = false) String searchTerm) {
+        List<Job> searchResults = new ArrayList<>();
+        if ( searchTerm == null) {
+            searchResults = JobData.findAll();
+        } else if (searchType.equals("all")){
+            searchResults = JobData.findByValue(searchTerm);
+        } else {
+            searchResults = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+    }
 }
 
